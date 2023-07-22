1697020b79751e9eab7405b614ae4db68c94b274
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-07-22 11:22:57 -0400

    Update Prepare-RBAC-ACR.ps1

--- Azure-Pre-Reqs/AzureContainerRegistry/Prepare-RBAC-ACR.ps1
+++ Azure-Pre-Reqs/AzureContainerRegistry/Prepare-RBAC-ACR.ps1
@@ -3,7 +3,7 @@
 
 # Setup Variables. (provide your ACR name)
 $appName = "GitHub-ACI-Deploy"
-$acrName = "<ACRName>"
+$acrName = "librechatregistry1870"
 
 # Create AAD App and Service Principal and assign to RBAC Role to push and pull images from ACR
 $acrId = az acr show --name "$acrName" --query id --output tsv
