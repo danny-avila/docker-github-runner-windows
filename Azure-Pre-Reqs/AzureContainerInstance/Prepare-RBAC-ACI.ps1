91ef968e9431b2ed9a00b13eb5098c80b8d101a2
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-07-22 11:37:19 -0400

    Update Prepare-RBAC-ACI.ps1

--- Azure-Pre-Reqs/AzureContainerInstance/Prepare-RBAC-ACI.ps1
+++ Azure-Pre-Reqs/AzureContainerInstance/Prepare-RBAC-ACI.ps1
@@ -3,8 +3,8 @@
 
 # Setup Variables.
 $aciResourceGroupName = "Demo-ACI-GitHub-Runners-RG"
-$appName = "GitHub-ACI-Deploy" #Previously created Service Principal (See part 3 of blog series)
-$region = "uksouth"
+$appName = "LibreChat" #Previously created Service Principal (See part 3 of blog series)
+$region = "eastus"
 
 # Create a resource group to deploy ACIs to
 az group create --name "$aciResourceGroupName" --location "$region"
